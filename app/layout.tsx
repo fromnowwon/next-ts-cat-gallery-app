b5ed5c1c2221fbea478700da3740f3e8aa390ad1
cw <fromnowwon@gmail.com>
2024-07-15 16:46:37 +0900

    style: theme, fontfamily 정의

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,25 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
+  fontFamily: {
+    sans: ["Montserrat", "sans-serif"],
+  },
+  container: {
+    padding: "0 1rem",
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: {
+        DEFAULT: "#FF6A13",
+        dark: "#C75B0A",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
