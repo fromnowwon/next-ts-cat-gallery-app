cddd30b6aaf5cce7fbc0802bcf1dce4472d70584
cw <fromnowwon@gmail.com>
2024-07-15 17:23:52 +0900

    feat: 즐겨찾기 버튼 생성

--- components/common/CatCard.tsx
+++ components/common/CatCard.tsx
@@ -1,14 +1,22 @@
 import Image from "next/image";
 import { Cat } from "@/types/CatTypes";
+import { BsHeartFill, BsHeart } from "react-icons/bs";
+import { useCatStore } from "@/store/catStore";
 
 interface CatCardProps {
   cat: Cat;
 }
 
 export default function CatCard({ cat }: CatCardProps) {
+  const { favoriteCats, toggleFavorite } = useCatStore();
+  const isFavorite = favoriteCats.some((fav) => fav.id === cat.id);
+
   return (
     <div>
       <Image src={`${cat.url}`} alt={cat.id} width={300} height={400} />
+      <button onClick={() => toggleFavorite(cat)}>
+        {isFavorite ? <BsHeartFill /> : <BsHeart />}
+      </button>
     </div>
   );
 }
--- store/catStore.ts
+++ store/catStore.ts
@@ -4,13 +4,26 @@ import { create } from "zustand";
 
 interface CatState {
   cats: Cat[];
+  favoriteCats: Cat[];
   fetchCats: () => void;
+  toggleFavorite: (cat: Cat) => void;
 }
 
 export const useCatStore = create<CatState>((set) => ({
   cats: [],
+  favoriteCats: [],
   fetchCats: async () => {
     const catsData = await fetchCats();
     set({ cats: catsData });
   },
+  toggleFavorite: (cat) =>
+    set((state) => {
+      const isFavorite = state.favoriteCats.some((fav) => fav.id === cat.id);
+
+      return {
+        favoriteCats: isFavorite
+          ? state.favoriteCats.filter((fav) => fav.id !== cat.id)
+          : [...state.favoriteCats, cat],
+      };
+    }),
 }));
